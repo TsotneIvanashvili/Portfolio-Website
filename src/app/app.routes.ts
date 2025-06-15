cd1bfc79ba551553c849cff74b5e979637a41be3
Tsotne Ivanashvili <tsotneivanashvili@gmail.com>
2025-06-15 04:24:01 -0700

    main

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: AboutComponent},
+    {path: "resume", component: ResumeComponent},
+    {path: "portfolio", component: ProjectsComponent},
+    {path: "contact", component: ContactComponent},
+];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
-import { RouterModule } from '@angular/router';
-import { BannerComponent } from "../banner/banner.component";
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, RouterModule, BannerComponent],
+  imports: [RouterModule, NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  public email = 'Tsotneivanashvili@gmail.com'
+
 
 }
