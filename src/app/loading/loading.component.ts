c1c3cb7079c7d9bb5610a53bf0f5658484d4c544
Tsotne Ivanashvili <tsotneivanashvili@gmail.com>
2025-06-19 00:57:23 -0700

    main

--- src/app/loading/loading.component.ts
+++ src/app/loading/loading.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./loading.component.css']
 })
 export class LoadingComponent implements OnInit {
-  originalText = 'We...';
+  originalText = 'Welcome...';
   subOriginalText = 'Please wait...';
   displayText = '';
   subDisplayText = '';
